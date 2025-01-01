2eab7c719029ca1fc4e8cee2cad26143f180067f
MichalMitros <mitrosmicha4@gmail.com>
2025-01-01 22:26:59 +0100

    feat: add scoreboard for storing and updating matches

--- scoreboard/scoreboard.go
+++ scoreboard/scoreboard.go
@@ -1,5 +1,156 @@
-// Package scoreboard implements score board for storing games and scores.
+// Package scoreboard implements score board for storing games and scores
+// and handling events updating their state.
 package scoreboard
 
-// ScoreBoard stores matches.
-type ScoreBoard struct{}
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+)
+
+// EventType represents types of events.
+type EventType string
+
+// Event types.
+const (
+	StartEvent  EventType = "start"
+	FinishEvent EventType = "finish"
+	UpdateEvent EventType = "update"
+)
+
+// Match is single ongoing match with scores.
+type Match struct {
+	homeTeam  string
+	awayTeam  string
+	homeScore int
+	awayScore int
+	startTime time.Time
+}
+
+// Event modifies state of Match.
+type Event struct {
+	Type      EventType
+	HomeTeam  string
+	AwayTeam  string
+	HomeScore int
+	AwayScore int
+}
+
+// ScoreBoard stores matches and handles events.
+type ScoreBoard struct {
+	matches map[string]*Match
+}
+
+// New returns new ScoreBoard.
+func New() *ScoreBoard {
+	return &ScoreBoard{
+		matches: make(map[string]*Match),
+	}
+}
+
+// HandleEvent handles start, update and finish events.
+func (s *ScoreBoard) HandleEvent(event Event) error {
+	switch event.Type {
+	case StartEvent:
+		return s.startGame(event.HomeTeam, event.AwayTeam)
+	case FinishEvent:
+		return s.finishGame(event.HomeTeam, event.AwayTeam)
+	case UpdateEvent:
+		return s.updateScore(event.HomeTeam, event.AwayTeam, event.HomeScore, event.AwayScore)
+	default:
+		return ErrUnknownEventType
+	}
+}
+
+// startGame starts new game.
+func (s *ScoreBoard) startGame(homeTeam, awayTeam string) error {
+	key := buildGameKey(homeTeam, awayTeam)
+	if _, exists := s.matches[key]; exists {
+		return ErrGameAlreadyOngoing
+	}
+
+	// Check if teams are not currently playing.
+	for _, match := range s.matches {
+		if homeTeam == match.homeTeam || homeTeam == match.awayTeam {
+			return ErrHomeTeamAlreadyPlaying
+		}
+		if awayTeam == match.homeTeam || awayTeam == match.awayTeam {
+			return ErrAwayTeamAlreadyPlaying
+		}
+	}
+
+	s.matches[key] = &Match{
+		homeTeam:  homeTeam,
+		awayTeam:  awayTeam,
+		homeScore: 0,
+		awayScore: 0,
+		startTime: time.Now(),
+	}
+
+	return nil
+}
+
+// finishGame finishes ongoing game.
+func (s *ScoreBoard) finishGame(homeTeam, awayTeam string) error {
+	key := buildGameKey(homeTeam, awayTeam)
+	if _, exists := s.matches[key]; !exists {
+		return ErrGameNotFound
+	}
+	delete(s.matches, key)
+
+	return nil
+}
+
+// updateScore updates scores of ongoing game.
+func (s *ScoreBoard) updateScore(homeTeam, awayTeam string, homeScore, awayScore int) error {
+	if homeScore < 0 || awayScore < 0 {
+		return ErrNegativeScore
+	}
+
+	key := buildGameKey(homeTeam, awayTeam)
+	match, exists := s.matches[key]
+	if !exists {
+		return ErrGameNotFound
+	}
+	match.homeScore = homeScore
+	match.awayScore = awayScore
+
+	return nil
+}
+
+// GetSummary returns summary of ScoreBoard as string describing scores in ongoing games.
+func (s *ScoreBoard) GetSummary() string {
+	matches := make([]*Match, 0, len(s.matches))
+	for _, match := range s.matches {
+		if match != nil {
+			matches = append(matches, match)
+		}
+	}
+
+	sort.SliceStable(matches, func(i, j int) bool {
+		totalScoreI := matches[i].homeScore + matches[i].awayScore
+		totalScoreJ := matches[j].homeScore + matches[j].awayScore
+		if totalScoreI == totalScoreJ {
+			return matches[i].startTime.Before(matches[j].startTime)
+		}
+		return totalScoreI > totalScoreJ
+	})
+
+	var summary strings.Builder
+	// WriteString never retruns any error.
+	_, _ = summary.WriteString("Game Summary:\n")
+
+	for ix, match := range matches {
+		_, _ = summary.WriteString(
+			fmt.Sprintf("%d. %s %d - %s %d\n", ix+1, match.homeTeam, match.homeScore, match.awayTeam, match.awayScore),
+		)
+	}
+
+	return summary.String()
+}
+
+// buildGameKey builds map key for the game.
+func buildGameKey(homeTeam, awayTeam string) string {
+	return fmt.Sprintf("%s vs %s", homeTeam, awayTeam)
+}
---
+++ scoreboard/scoreboard_test.go
@@ -0,0 +1,143 @@
+package scoreboard_test
+
+import (
+	"testing"
+
+	"github.com/MichalMitros/sportradar-task/scoreboard"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnitNew(t *testing.T) {
+	assert.NotNil(t, scoreboard.New(), "should return new ScoreBoard")
+}
+
+func TestUnitHandleEvent(t *testing.T) {
+	const unknownEventType scoreboard.EventType = "unknown"
+	tests := map[string]struct {
+		events         []scoreboard.Event
+		expectedErrors []error // must be the same length as events.
+	}{
+		"game not found error": {
+			events: []scoreboard.Event{
+				{Type: scoreboard.UpdateEvent, HomeTeam: "A", AwayTeam: "B"},
+				{Type: scoreboard.FinishEvent, HomeTeam: "C", AwayTeam: "D"},
+			},
+			expectedErrors: []error{
+				scoreboard.ErrGameNotFound,
+				scoreboard.ErrGameNotFound,
+			},
+		},
+		"negative score error": {
+			events: []scoreboard.Event{
+				{Type: scoreboard.StartEvent, HomeTeam: "A", AwayTeam: "B"},
+				{Type: scoreboard.UpdateEvent, HomeTeam: "A", AwayTeam: "B", HomeScore: -1},
+			},
+			expectedErrors: []error{
+				nil,
+				scoreboard.ErrNegativeScore,
+			},
+		},
+		"game already ongoing error": {
+			events: []scoreboard.Event{
+				{Type: scoreboard.StartEvent, HomeTeam: "A", AwayTeam: "B"},
+				{Type: scoreboard.StartEvent, HomeTeam: "A", AwayTeam: "B"},
+			},
+			expectedErrors: []error{
+				nil,
+				scoreboard.ErrGameAlreadyOngoing,
+			},
+		},
+		"home team already playing error": {
+			events: []scoreboard.Event{
+				{Type: scoreboard.StartEvent, HomeTeam: "A", AwayTeam: "B"},
+				{Type: scoreboard.StartEvent, HomeTeam: "A", AwayTeam: "C"},
+			},
+			expectedErrors: []error{
+				nil,
+				scoreboard.ErrHomeTeamAlreadyPlaying,
+			},
+		},
+		"away team already playing error": {
+			events: []scoreboard.Event{
+				{Type: scoreboard.StartEvent, HomeTeam: "A", AwayTeam: "B"},
+				{Type: scoreboard.StartEvent, HomeTeam: "C", AwayTeam: "B"},
+			},
+			expectedErrors: []error{
+				nil,
+				scoreboard.ErrAwayTeamAlreadyPlaying,
+			},
+		},
+		"unknown event type": {
+			events: []scoreboard.Event{
+				{Type: unknownEventType, HomeTeam: "A", AwayTeam: "B"},
+			},
+			expectedErrors: []error{
+				scoreboard.ErrUnknownEventType,
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			scoreBoard := scoreboard.New()
+			errors := make([]error, len(tt.events))
+
+			for ix, event := range tt.events {
+				errors[ix] = scoreBoard.HandleEvent(event)
+			}
+
+			assert.Equal(t, tt.expectedErrors, errors, "should return correct errors")
+		})
+	}
+}
+
+func TestUnitGetSummary(t *testing.T) {
+	tests := map[string]struct {
+		events   []scoreboard.Event
+		expected string
+	}{
+		"no games": {
+			expected: "Game Summary:\n",
+		},
+		"no ongoing games": {
+			events: []scoreboard.Event{
+				{
+					Type:     scoreboard.StartEvent,
+					HomeTeam: "A",
+					AwayTeam: "B",
+				},
+				{
+					Type:     scoreboard.FinishEvent,
+					HomeTeam: "A",
+					AwayTeam: "B",
+				},
+			},
+			expected: "Game Summary:\n",
+		},
+		"many games": {
+			events: []scoreboard.Event{
+				{Type: scoreboard.StartEvent, HomeTeam: "A", AwayTeam: "B"},
+				{Type: scoreboard.StartEvent, HomeTeam: "C", AwayTeam: "D"},
+				{Type: scoreboard.UpdateEvent, HomeTeam: "A", AwayTeam: "B", HomeScore: 1, AwayScore: 2},
+				{Type: scoreboard.StartEvent, HomeTeam: "E", AwayTeam: "F"},
+				{Type: scoreboard.UpdateEvent, HomeTeam: "E", AwayTeam: "F", HomeScore: 1, AwayScore: 2},
+				{Type: scoreboard.UpdateEvent, HomeTeam: "C", AwayTeam: "D", HomeScore: 2, AwayScore: 3},
+			},
+			expected: "Game Summary:\n1. C 2 - D 3\n2. A 1 - B 2\n3. E 1 - F 2\n",
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			scoreBoard := scoreboard.New()
+
+			for _, event := range tt.events {
+				err := scoreBoard.HandleEvent(event)
+				require.NoError(t, err)
+			}
+
+			assert.Equal(t, tt.expected, scoreBoard.GetSummary(), "should return correct summary")
+		})
+	}
+}
