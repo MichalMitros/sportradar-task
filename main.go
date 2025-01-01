a6be6c7e56116dc36c109b3cb347fdd305cb4f03
MichalMitros <mitrosmicha4@gmail.com>
2025-01-01 23:08:53 +0100

    feat: add commands to provide both ui and demo

--- main.go
+++ main.go
@@ -2,15 +2,79 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/MichalMitros/sportradar-task/scoreboard"
 	"github.com/MichalMitros/sportradar-task/ui"
 )
 
+//nolint:gochecknoglobals // it's ok for simple demonstration purposes
+var commands map[string]struct {
+	Description string
+	Behavior    func()
+}
+
+func init() {
+	commands = map[string]struct {
+		Description string
+		Behavior    func()
+	}{
+		"help": {
+			Description: "Show available commands.",
+			Behavior:    printHelp,
+		},
+		"ui": {
+			Description: "Run score board with user interface. (default)",
+			Behavior:    runUI,
+		},
+		"demo": {
+			Description: "Run simple demo without user interface.",
+			Behavior:    runDemo,
+		},
+	}
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		commands["ui"].Behavior()
+		return
+	}
+
+	for cmd, action := range commands {
+		if os.Args[1] == cmd {
+			action.Behavior()
+			return
+		}
+	}
+
+	printHelp()
+}
+
+// printHelp prints commands and their descriptions.
+func printHelp() {
+	fmt.Println("Available commands:")
+	for cmd, action := range commands {
+		fmt.Printf("%s\t-\t%s\n", cmd, action.Description)
+	}
+}
+
+// runUI runs score board in interactive mode with simple user interface.
+func runUI() {
 	scoreBoard := scoreboard.New()
 	scoreBoard.Use(scoreboard.LoggingMiddleware)
 
+	view := ui.New(scoreBoard)
+	for {
+		view.Show()
+	}
+}
+
+// runDemo runs simple demo with some mocked events for quick demonstration.
+func runDemo() {
+	scoreBoard := scoreboard.New()
+	scoreBoard.Use(scoreboard.LoggingMiddleware)
+
+	// Errors from HandleEvent will be logged and doesn't need to be handled here.
 	_ = scoreBoard.HandleEvent(scoreboard.Event{Type: scoreboard.StartEvent, HomeTeam: "Mexico", AwayTeam: "Canada"})
 	_ = scoreBoard.HandleEvent(scoreboard.Event{Type: scoreboard.StartEvent, HomeTeam: "Spain", AwayTeam: "Brazil"})
 	_ = scoreBoard.HandleEvent(scoreboard.Event{Type: scoreboard.StartEvent, HomeTeam: "Germany", AwayTeam: "France"})
@@ -42,12 +106,4 @@ func main() {
 	_ = scoreBoard.HandleEvent(scoreboard.Event{Type: scoreboard.FinishEvent, HomeTeam: "Germany", AwayTeam: "France"})
 
 	fmt.Println(scoreBoard.GetSummary())
-
-	scoreBoard = scoreboard.New()
-	scoreBoard.Use(scoreboard.LoggingMiddleware)
-
-	view := ui.New(scoreBoard)
-	for {
-		view.Show()
-	}
 }
